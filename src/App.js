9deda6cca2475b3e0bf7434dfb4386b31a1212a7
Yashraj Jain <yashrajjain726@gmail.com>
2021-03-31 16:50:28 +0530

    hooks implementation  done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Movie from './hoc/Movie'
 import Counter from './hooks/Counter'
+import Users from './hooks/Users';
 function App() {
   return (
     // <Movie id={1}/>.
-    <Counter/>
+    // <Counter/>
+    <Users/>
   );
 }
 
